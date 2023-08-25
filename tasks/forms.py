e6433272c9ca6aa57281eee4e2045fc87ed54803
sahilmiddya <middyasahil00@gmail.com>
2023-08-25 12:05:15 +0530

    initial commit

---
+++ tasks/forms.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*-
+from .models import Task
+from django import forms
+
+
+class TaskForm(forms.ModelForm):
+
+    class Meta:
+        model = Task
+        fields = "__all__"
